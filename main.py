7360213f8bda3380eec78f80204a74d76233924e
Spencer Miller <spencermiller90@yahoo.com>
2022-06-14 20:58:10 -0500

    refactored to using discords app_command library

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 from os import getenv
 
@@ -9,7 +9,56 @@ token = getenv("TOKEN")
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="!")
-bot.load_extension("bot_commands")
+GUILD = discord.Object(id=978803693820452874)
 
-bot.run(token)
+
+class Client(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync(guild=GUILD)
+            self.synced = True
+
+
+client = Client()
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(
+    name="role_call",
+    description="This command snapshots the members of a specific VC.",
+    guild=GUILD
+)
+async def self(interaction: discord.Interaction, channel_id: str):
+    voice_channel = client.get_channel(int(channel_id))
+    role_call = get_vc_members(voice_channel)
+    await interaction.response.send_message(embed=embed_message(
+        vc_members=role_call,
+        channel=voice_channel)
+    )
+
+
+def get_vc_members(voice_channel):
+    member_list = []
+    for member in voice_channel.members:
+        member_list.append(member.name)
+    if len(member_list) <= 0:
+        return "Channel is empty"
+    member_per_line = '\n'.join(member_list)
+    return member_per_line
+
+
+def embed_message(vc_members, channel):
+    embed = discord.Embed(
+        title=f"{channel}",
+        color=discord.Color.green()
+    )
+    embed.add_field(name="Members", value='>>> {}'.format(vc_members))
+    return embed
+
+
+client.run(token)
